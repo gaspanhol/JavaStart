038fe9a3f5e7248c3875479a62daf63b56deb54e
spanhol <160233232+gaspanhol@users.noreply.github.com>
2025-09-18 23:18:13 +0000

    aula 2 sobre java

---
+++ App.java
@@ -0,0 +1,16 @@
+public class App {
+    public static void main(String[] args) {
+        
+        // Operadores aritméticos: +, -, /, *, %(mod)
+        
+        int a = 1; 
+        int b = 2;
+
+        double resultado = a / (double) b;
+
+        System.out.println(resultado);
+
+        // System.out.printf("%.2f\n", resultado);  //o 5f são a quantidade de casas depois da vírgula
+
+    }
+}
